057aa833b15b39da02311dd2ec3de3533e5aefb8
Malcolm Fischer <mtfische@ualberta.ca>
2018-01-31 15:53:39 -0700

    removed useless files

--- follow_bot/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/malcolm/turtlebot_ws/src/follow_bot
-# Build directory: /home/malcolm/turtlebot_ws/src/follow_bot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
