af43d071973bc274995285a5702b3d0c1d1f5638
Adam Cawthorn <adamtcawt@gmail.com>
2018-02-01 13:26:38 -0700

    add kill switch

--- follow_bot/move_to_centroid.cpp
+++ follow_bot/move_to_centroid.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "sensor_msgs/Joy.h"
 #include <geometry_msgs/Twist.h>
 #include <pcl/common/centroid.h>
 #include <pcl_ros/point_cloud.h>
@@ -11,6 +12,9 @@
 float x = 0;
 float y = 0;
 float z = 0;
+
+// Are we moving?
+int moving = false;
 
 void centroid_callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& msg) {
   // centroid: x=l/r, y=u/d, z:distance
@@ -27,14 +31,21 @@ void centroid_callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& msg) {
   z = centroid.x;
 }
 
+void killswitch_callback(const sensor_msgs::Joy::ConstPtr & msg) {
+  if(msg->buttons[2] == 1) {
+    moving = true;
+  }
+  else if(msg->buttons[1])
+    moving = false;
+}
+
 int main(int argc, char **argv) {
   // Constants (magic numbers):
-  int stop_distance = 1;
-  int x_scale = 0.5;
-  int z_scale = 0.5;
-
-
-  int stopMoving = false;
+  float stop_distance = 1;
+  float x_scale = 0.2;
+  float z_scale = 0.2;
+  float x_threshold = 0.003;
+  float z_threshold = 0.003;
 
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
@@ -75,23 +86,36 @@ int main(int argc, char **argv) {
   ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
   ros::Subscriber centroid_sub = n.subscribe("target_cluster", 1, centroid_callback);
 
+  //rosrun joy y_node
+  ros::Subscriber joystick_sub = n.subscribe("joy", 10, killswitch_callback);
+
   ros::Rate rate(10);
   while (ros::ok()) {
-    if (!stopMoving) {
+    if (moving) {
       // Get centroid we are navigating to
-
-
       geometry_msgs::Twist vel_msg;
       // Calculate V
       if(x != 0) {
-        vel_msg.linear.x = (x - stop_distance) * x_scale;
+        if(abs(x- stop_distance) * x_scale > x_threshold) {
+          vel_msg.linear.x = (x - stop_distance) * x_scale;
+        }
+      }
+      else {
+        // stop
+        vel_msg.linear.x = 0;
       }
 
       // Calculate omega (w)
-      vel_msg.angular.z = -z * z_scale;
+      if(abs(z) * z_scale > z_threshold ) {
+        vel_msg.angular.z = -z * z_scale;
+      }
+      else {
+        // stop
+        vel_msg.angular.z = 0;
+      }
 
       // Publish Twist
-      std::cout << "x: " << x << "\ny: " << y << "\ny: " << y << "\n";
+      std::cout << "x: " << x << "\ny: " << y << "\nz: " << z << "\n";
       std::cout << "linear x: " << vel_msg.linear.x << "\nangular z: " << vel_msg.angular.z << "\n\n\n";
       cmd_vel_pub.publish(vel_msg);
 
